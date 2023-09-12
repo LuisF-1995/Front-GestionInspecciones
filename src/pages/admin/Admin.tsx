9d177b37a4c07e75f10607b790bab03fe50f4fae
LuisF-1995 <luis-1995@live.com.ar>
2023-09-12 16:56:20 -0500

    Primer cargue

---
+++ src/pages/admin/Admin.tsx
@@ -0,0 +1,36 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { API_GESTION_INSPECCIONES_URL } from '@/constants/GlobalConstants';
+import { sendGetRequest } from '@/services/GestionInspeccionesApiData';
+
+const Admin = () => {
+    const ADMIN_URL = `${API_GESTION_INSPECCIONES_URL}/admin`;
+    const [statusOptions, setStatusOptions] = useState([]);
+
+    useEffect(() => {
+        getStatusOptions();
+        return 
+    }, [])
+
+    const getStatusOptions = () => {
+        sendGetRequest(`${ADMIN_URL}/status-options`)
+        .then((status) => {
+            setStatusOptions(status);
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+    }
+    
+    return (
+        <section>
+            <select name="statusSelector" id="statusSelectorId">
+                {statusOptions.map((status, index) => (
+                    <option key={index} value={status}>{status}</option>
+                ))}
+            </select>
+        </section>
+    )
+}
+
+export default Admin
