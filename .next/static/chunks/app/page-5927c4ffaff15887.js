1f3da995c80040ba2ae678e55101711997413a4d
LuisF-1995 <luis-1995@live.com.ar>
2023-11-13 17:45:43 -0500

    Prueba desplieuge en vercel

--- .next/static/chunks/app/page-5927c4ffaff15887.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{3483:function(e,r,n){Promise.resolve().then(n.bind(n,5003))},5003:function(e,r,n){"use strict";n.r(r),n.d(r,{default:function(){return c}});var t=n(7437),s=n(1396),i=n.n(s);function c(){return(0,t.jsxs)(t.Fragment,{children:[(0,t.jsx)("h1",{children:"Home page"}),(0,t.jsx)("nav",{children:(0,t.jsxs)("ul",{children:[(0,t.jsx)("li",{children:(0,t.jsx)(i(),{href:"./company/admin",children:"Admin"})}),(0,t.jsx)("li",{children:(0,t.jsx)(i(),{href:"./company",children:"Empresa"})}),(0,t.jsx)("li",{children:(0,t.jsx)(i(),{href:"./cliente",children:"Clientes"})}),(0,t.jsx)("li",{children:(0,t.jsx)(i(),{href:"./constructor",children:"Constructores"})})]})})]})}},622:function(e,r,n){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var t=n(2265),s=Symbol.for("react.element"),i=Symbol.for("react.fragment"),c=Object.prototype.hasOwnProperty,o=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,l={key:!0,ref:!0,__self:!0,__source:!0};function f(e,r,n){var t,i={},f=null,u=null;for(t in void 0!==n&&(f=""+n),void 0!==r.key&&(f=""+r.key),void 0!==r.ref&&(u=r.ref),r)c.call(r,t)&&!l.hasOwnProperty(t)&&(i[t]=r[t]);if(e&&e.defaultProps)for(t in r=e.defaultProps)void 0===i[t]&&(i[t]=r[t]);return{$$typeof:s,type:e,key:f,ref:u,props:i,_owner:o.current}}r.Fragment=i,r.jsx=f,r.jsxs=f},7437:function(e,r,n){"use strict";e.exports=n(622)}},function(e){e.O(0,[396,971,596,744],function(){return e(e.s=3483)}),_N_E=e.O()}]);
